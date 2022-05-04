1c6df6d18bcc92538bd68374d29360e9edc78217
Ilyas Bezhanidze <iljas.bezhanidze@totonis.com>
2022-05-04 08:42:02 +0300

    register

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -14,7 +14,8 @@
 //EDUCATION WORKS
 // import myComponents from './training-works/1_myImageEditor.vue'
 // import myComponents from './training-works/2_toggleVisible&title'
-import myComponents from './training-works/3_arrayMethods'
+// import myComponents from './training-works/3_arrayMethods'
+import myComponents from './training-works/4_stringRegister'
 
 export default {
   name: 'App',
---
+++ vue/src/training-works/4_stringRegister.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="str">
+    <button @click="toggleRegister">Змінити</button>
+    <h1>{{ text }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      text: "register",
+      flag: false,
+    };
+  },
+  methods: {
+    toggleRegister() {
+      this.flag = !this.flag;
+      this.getFlag();
+    },
+
+    getFlag() {
+      this.flag ? this.change() : (this.text = this.text.toLowerCase());
+    },
+
+    upperCaseFirst() {
+      this.text = this.text[0].toUpperCase() + this.text.substring(1);
+    },
+
+    upperCaseLast() {
+      this.text = this.text.replace(
+        /.{1}$/,
+        `${this.text.slice(-1).toUpperCase()}`
+      );
+    },
+
+    change() {
+      this.upperCaseFirst();
+      this.upperCaseLast();
+    },
+  },
+};
+</script>
